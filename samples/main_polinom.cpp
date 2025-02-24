b912da637622f9a0b3f7deb5d459d39ee3fd06b3
arasash1 <zhdankin228@gmail.com>
2025-02-24 11:25:11 +0300

    Update main_polinom.cpp

--- samples/main_polinom.cpp
+++ samples/main_polinom.cpp
@@ -6,9 +6,9 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	Polynom poly1("3x1y2z1 + 2x2y2z1");
-	Polynom poly2("3x1y2z1 + 2x2y2z3");
-	Monom mon1(10, 2, 4, 6);
+	Polynom poly1("3x2y2z2 + 10x5y5z5");
+	Polynom poly2("0x1y2z1 + 0x2y2z3");
+	Monom mon1(10, 2, 3, 30);
 	cout << "Полином 1:" << endl;
 	poly1.printPol();
 	cout << "Полином 2:" << endl;
